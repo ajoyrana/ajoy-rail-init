473ce36e38a071e9e1f64bf44f07b0c505343f72
Ajoy Kumar Rana <ajoyrana@yahoo.com>
2013-09-20 12:12:42 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjoyRailInit::Application.config.secret_token = 'de71593457b7280a8e10df70c577b54e6b128c6a7b3b5128845568509b9cb4b5dc2c167367519a803d181c2a6a392188420484acafe228e15b8f47dae1786e30'
